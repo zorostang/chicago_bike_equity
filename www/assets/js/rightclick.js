5c491b4cf5230d3794bc52424b1c53fadb367f00
Robert Guico <rguico@gmail.com>
2015-03-19 15:55:01 -0500

    Find the nearest 5 Divvy stations on right-click, by cheating with turf-remove. Well, at least it seems fast.

--- www/assets/js/rightclick.js
+++ www/assets/js/rightclick.js
@@ -3,11 +3,11 @@ var nearestLayer, nearest, point;
 function findNearbyDivvy(e) {
 	console.log(e.latlng);
 	console.log(e);
-	
+
 	if(map.hasLayer(nearestLayer) ) {
 		map.removeLayer(nearestLayer);
 	}
-	
+
 	point = {
 		  "type": "Feature",
 		  "properties": {
@@ -20,27 +20,18 @@ function findNearbyDivvy(e) {
 		};
 	console.log("Point: ");
 	console.log(point);
-		
-	nearest = turf.nearest( point, divvyStations.toGeoJSON() );
-	console.log(nearest);
-	nearestLayer = L.geoJson(nearest).addTo(map);
-	
-	/*
-var geojsonMarkerOptions = {
-	    radius: 8,
-	    fillColor: "#ff7800",
-	    color: "#000",
-	    weight: 1,
-	    opacity: 1,
-	    fillOpacity: 0.8
-	};
-	
-	L.geoJson(nearest, {
-	    pointToLayer: function (feature, latlng) {
-	        return L.circleMarker(latlng, geojsonMarkerOptions);
-	    }
-	}).addTo(map);
-*/
+
+	nearestLayer = L.geoJson(null);
+	var divvyStationsFC = divvyStations.toGeoJSON();
+
+	for (var x = 0; x < 5; x++) {
+		var nextNearest = turf.nearest( point, divvyStationsFC );
+		var id = nextNearest.properties.ID;
+		divvyStationsFC = turf.remove(divvyStationsFC, 'ID', id);
+		nearestLayer.addData(nextNearest);
+	}
+
+	nearestLayer.addTo(map);
 }
 
 function showCoordinates (e) {
