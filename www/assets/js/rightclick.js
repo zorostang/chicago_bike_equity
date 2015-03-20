57d0e17dc2c38c3ff2555160bb9eeeeb7bc25f3c
Robert Guico <rguico@gmail.com>
2015-03-19 23:39:54 -0500

    Improved right click functionality (lines are drawn, labels are created).

--- www/assets/js/rightclick.js
+++ www/assets/js/rightclick.js
@@ -1,6 +1,12 @@
-var nearestLayer, nearest, point;
+/* jshint unused: false */
+/* global console, map, divvyStations, L, turf, $ */
+
+	var lines = [];
+	var nearestLayer, nearest, point, initialMarker;
 
 function findNearbyDivvy(e) {
+	'use strict';
+
 	console.log(e.latlng);
 	console.log(e);
 
@@ -9,33 +15,62 @@ function findNearbyDivvy(e) {
 	}
 
 	point = {
-		  "type": "Feature",
-		  "properties": {
-		    "marker-color": "#0f0"
+		  'type': 'Feature',
+		  'properties': {
+		    'marker-color': '#0f0'
 		  },
-		  "geometry": {
-		    "type": "Point",
-		    "coordinates": [e.latlng.lng, e.latlng.lat]
+		  'geometry': {
+		    'type': 'Point',
+		    'coordinates': [e.latlng.lng, e.latlng.lat]
 		  }
 		};
-	console.log("Point: ");
+	console.log('Point: ');
 	console.log(point);
 
-	nearestLayer = L.geoJson(null);
 	var divvyStationsFC = divvyStations.toGeoJSON();
+
+	var redMarker = L.AwesomeMarkers.icon({
+		icon: 'star',
+		markerColor: 'red'
+	});
+
+	if (initialMarker) {
+		initialMarker.setLatLng([e.latlng.lat, e.latlng.lng]);
+		initialMarker.update();
+	} else {
+		initialMarker = L.marker([e.latlng.lat, e.latlng.lng], {icon: redMarker});
+		initialMarker.addTo(map);
+	}
+
+	nearestLayer = L.geoJson(null);
 
 	for (var x = 0; x < 5; x++) {
 		var nextNearest = turf.nearest( point, divvyStationsFC );
 		var id = nextNearest.properties.ID;
 		divvyStationsFC = turf.remove(divvyStationsFC, 'ID', id);
 		nearestLayer.addData(nextNearest);
-	}
 
+		if (lines[x]) {
+			lines[x].setLatLngs([[nextNearest.geometry.coordinates[1], nextNearest.geometry.coordinates[0]], [e.latlng.lat, e.latlng.lng]]);
+			setDistanceLabel(lines[x]);
+		} else {
+			lines[x] = L.polyline([[nextNearest.geometry.coordinates[1], nextNearest.geometry.coordinates[0]], [e.latlng.lat, e.latlng.lng]]);
+			setDistanceLabel(lines[x]).addTo(map);
+		}
+	}
 	nearestLayer.addTo(map);
+	map.fitBounds(nearestLayer);
+}
+
+function setDistanceLabel(line) {
+	'use strict';
+	var dist = turf.lineDistance(line.toGeoJSON(), 'miles');
+	return line.bindLabel(Math.round(dist * 100) / 100 + ' mi');
 }
 
 function showCoordinates (e) {
+	'use strict';
 	//remove the previous coordinate information, if it exists
-	$(".coordinate").remove();
-	$("#features").append("<div class='panel-heading coordinate'> Cooordinates: "  + e.latlng + "</div>");
+	$('.coordinate').remove();
+	$('#features').append('<div class="panel-heading coordinate"> Cooordinates: '  + e.latlng + '</div>');
 }
