83fa882cb95c6f50e3bf9c9e04c38365a18ff463
Ife Ajiboye <ajiboye_ifedayo@yahoo.com>
2015-05-18 11:15:09 -0500

    prepend locations to sidebar list

--- www/assets/js/rightclick.js
+++ www/assets/js/rightclick.js
@@ -210,7 +210,7 @@ function showAddress (e) {
 			$(".right_click_instructions").hide();
 			$('.error').remove(); 
 
-			$('#features .sidebar-table').append('<div class="panel-heading coordinate iteration_' + iteration + '"><h4>'  + address + '</h4><div class="iteration_' + iteration + '_bike_lanes"></div><div class="iteration_' + iteration + '_hypertension"></div><div class="iteration_' + iteration + '_bike_racks"></div></div>');
+			$('#features .sidebar-table').prepend('<div class="panel-heading coordinate iteration_' + iteration + '"><h4>'  + address + '</h4><div class="iteration_' + iteration + '_bike_lanes"></div><div class="iteration_' + iteration + '_hypertension"></div><div class="iteration_' + iteration + '_bike_racks"></div></div>');
 			
             var marker = L.marker(cords);
 			marker.addTo(map);
